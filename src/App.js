24d7534da643abb16c4c4ec34d414b7a4bf54821
T-Hamel94 <tristan_hamel@hotmail.com>
2023-04-30 17:36:46 -0400

    Adding a background image to the home page

--- src/App.js
+++ src/App.js
@@ -7,29 +7,29 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import Blog from './pages/Blog'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Container, Row, Col } from 'react-bootstrap';
 import './styles/App.css';
 
 function App() {
   return (
-
-    <div className="App wrapper">
-
-        <BrowserRouter>
+    <div className="wrapper">
+      <BrowserRouter>
         <NavBar />
-
-        <div className="main-content">
-          <Routes>
-              <Route index element={<Home />} />
-              <Route path="about" element={<About/>} />
-              <Route path="blogs" element={<Blog />} />
-              <Route path="contact" element={<Contact />} />
-              <Route path="*" element={<NoPage />} />
-          </Routes>
-        </div>
-
-          <Footer />
-        </BrowserRouter>
-
+        <Container fluid className="main-content">
+          <Row className="h-100 w-100">
+            <Col className="d-flex flex-column">
+              <Routes>
+                <Route index element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="blogs" element={<Blog />} />
+                <Route path="contact" element={<Contact />} />
+                <Route path="*" element={<NoPage />} />
+              </Routes>
+            </Col>
+          </Row>
+        </Container>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
