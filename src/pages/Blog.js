9a43ec90bef3ac7df717b37248a401ce02a6f5c2
T-Hamel94 <tristan_hamel@hotmail.com>
2023-05-07 18:56:39 -0400

    Upgrading and testing some CSS on the blog and contact pages

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,24 +1,41 @@
 import '../styles/pages/Blog.css';
-import { Container } from 'react-bootstrap';
 
 const Blog = () => {
   return (
-      <div id="blog">
-          <Container>
-              <div className="row">
-                  <div className="col-12 text-center">
-                      <h1 className="mt-3">Blog</h1>
-                  </div>
-              </div>
-              <div className="row justify-content-center">
-                  <div className="col-12 text-center">
-                      <p>This is where I write about interesting technologies and general thoughts</p>
-                  </div>
-              </div>
-          </Container>
+      <div id ="blogContainer">      
+            <div id="headerContainer">
+                <div className="row m-0 ">
+                    <div className="col-12 text-center">
+                        <h1 className="mt-3">Blog</h1>
+                    </div>
+                </div>
+                <div className="row justify-content-center m-0">
+                    <div className="col-12 text-center ">
+                        <p>This is where I write about interesting technologies and general thoughts</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className = "container" id="articleContainer">
+                <div className='row article'>
+                    <div className='col-12'>
+                        <h2>Article name 1</h2>
+                        <span>Article date</span>
+                        <p>Article First few lines: </p>
+                    </div>
+                </div>
+
+                <div className='row article'>
+                    <div className='col-12'>
+                        <h2>Article name 2</h2>
+                        <span>Article date</span>
+                        <p>Article First few lines: </p>
+                    </div>
+                </div>
+            </div>
       </div>
   );
 };
 
-  
-  export default Blog;
+export default Blog;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,23 +7,25 @@ const Contact = () => {
 
     return (
         <div id="contact">
-        <Container>
-          <div class="row">
-            <div class="col-12 text-center">
-              <h1 id="mainHeader">{t('contact_header')}</h1>
+          <div className='container'>
+
+            <div class="row">
+              <div class="col-12 text-center">
+                <h1 id="mainHeader">{t('contact_header')}</h1>
+              </div>
             </div>
-          </div>
-          
-          <div class="row">
-            <div class="col-12">
-              <h3 id="contentHeader">
-                {t('content_header1')} <a href="mailto:Tristan_Hamel@hotmail.com">Tristan_Hamel@hotmail.com</a> 
-                {t('content_header2')} <a href="https://www.linkedin.com/in/tristan-hamel-qcca">Linked In</a>
-                {t('content_header3')}
-              </h3>
+            
+            <div class="row" id="contactContent">
+              <div class="col-12">
+                <h3 id="contentHeader">
+                  {t('content_header1')} <a href="mailto:Tristan_Hamel@hotmail.com">Tristan_Hamel@hotmail.com</a> 
+                  {t('content_header2')} <a href="https://www.linkedin.com/in/tristan-hamel-qcca">Linked In</a>
+                  {t('content_header3')}
+                </h3>
+              </div>
             </div>
+
           </div>
-        </Container>
         </div>
       );
   };
