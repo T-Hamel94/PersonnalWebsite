957ae47ee213db7fd3afb5479b56adfb100d3206
T-Hamel94 <tristan_hamel@hotmail.com>
2023-05-07 11:56:53 -0400

    Fixed responsiveness for contact me page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,21 +1,29 @@
 import './../styles/pages/Contact.css'
 import { useTranslation } from 'react-i18next';
+import { Container } from 'react-bootstrap';
 
 const Contact = () => {
   const { t } = useTranslation('contact');
 
     return (
-        <div id="contact" class="row">
-          <div class="col-12 text-center">
-            <h1 id="mainHeader">{t('contact_header')}</h1>
+        <div id="contact">
+        <Container>
+          <div class="row">
+            <div class="col-12 text-center">
+              <h1 id="mainHeader">{t('contact_header')}</h1>
+            </div>
           </div>
-          <div class="col-12">
-            <h3 id="contentHeader">
-              {t('content_header1')} <a href="mailto:Tristan_Hamel@hotmail.com">Tristan_Hamel@hotmail.com</a> 
-              {t('content_header2')} <a href="https://www.linkedin.com/in/tristan-hamel-qcca">Linked In</a>
-              {t('content_header3')}
-            </h3>
+          
+          <div class="row">
+            <div class="col-12">
+              <h3 id="contentHeader">
+                {t('content_header1')} <a href="mailto:Tristan_Hamel@hotmail.com">Tristan_Hamel@hotmail.com</a> 
+                {t('content_header2')} <a href="https://www.linkedin.com/in/tristan-hamel-qcca">Linked In</a>
+                {t('content_header3')}
+              </h3>
+            </div>
           </div>
+        </Container>
         </div>
       );
   };
