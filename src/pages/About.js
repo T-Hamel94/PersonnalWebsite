76f5125839a54058eb1760312a348aeeb98e0c11
T-Hamel94 <tristan_hamel@hotmail.com>
2023-05-10 18:14:59 -0400

    working on the contact page styling and logos

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,32 +13,36 @@ const About = () => {
           </div>
         </div>
 
-        <div class='container'>
+        <div class='container mt-3 mb-2'>
           <div class='row'>
-            <div class="col-4">
-              <p> I am Tristan Hamel, a passionate of IT, I am particulary intersted in backend developement, DevOps and network infrastructure. I also have a bachelor's degree in industrial relations from University Laval </p>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+              <p class="justify"> I am Tristan Hamel, a passionate of IT, I am particulary intersted in backend developement, DevOps and network infrastructure. I also have a bachelor's degree in industrial relations from University Laval </p>
             </div>
-            <div class="col-4">
+            <div class="col-lg-4 col-md-6 col-sm-12 text-center">
+              IMG HERE 1
             </div>
-            <div class="col-4">
-              <p>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-            </div>
-
-            <div class="col-4">
-            </div>
-            <div class="col-4">
-              <p>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-            </div>
-            <div class="col-4">
+            <div class="col-lg-4 col-md-6 col-sm-12">
+              <p class="justify">Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
             </div>
 
-            <div class="col-4">
-              <p>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+            <div class="col-lg-4 col-md-6 col-sm-12 text-center">
+              IMG HERE 2
             </div>
-            <div class="col-4">
+            <div class="col-lg-4 col-md-6 col-sm-12">
+              <p class="justify">Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
             </div>
-            <div class="col-4">
-              <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+            <div class="col-lg-4 col-md-6 col-sm-12 text-center">
+              IMG HERE 3
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-sm-12">
+              <p class="justify">Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12 text-center">
+              IMG HERE 4
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+              <p class="justify"> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
             </div>
           </div>
         </div>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,51 +7,64 @@ const Contact = () => {
     return (
         <div id="contact">
 
-            <div class="row m-0 p-0">
-              <div class="col-12 text-center m-0 p-0">
-                <h1 id="contactHeader">{t('contact_header')}</h1>
-              </div>
+          <div class="row m-0 p-0">
+            <div class="col-12 text-center m-0 p-0">
+              <h1 id="contactHeader">{t('contact_header')}</h1>
             </div>
+          </div>
+
+          <div class="row contactText m-0">
+            <div class="col-12 ">
+              <p> Do not hesitate to contact me, it will be my pleasure to discuss with you about IT opportunities and more!</p>
+            </div>
+          </div>
 
           <div className='container'>
 
-            <div class="row contactContent">
-              <div class="col-12">
+            <div class="row contactContent d-flex justify-content-between">
+              <div class="col-6">
                 <h4>
-                  Email: <a href="mailto:Tristan_Hamel@hotmail.com">Tristan_Hamel@hotmail.com</a> 
+                  <a href="mailto:Tristan_Hamel@hotmail.com">Tristan_Hamel@Hotmail.com</a> 
                 </h4>
+              </div>
+              <div class="col-6 d-flex justify-content-end">
+                <img src={process.env.PUBLIC_URL + "/logo/emailLogo.png"} class="contactLogo"></img>
               </div>
             </div>
 
-            <div class="row contactContent">
-              <div class="col-12">
+            <div class="row contactContent d-flex justify-content-between">
+              <div class="col-6 ">
                 <h4>
-                  Linked In: <a href="https://www.linkedin.com/in/tristan-hamel-qcca">Tristan Hamel</a> 
+                  <a href="https://www.linkedin.com/in/tristan-hamel-qcca">Linked In</a> 
                 </h4>
+              </div>
+              <div class="col-6 d-flex justify-content-end">
+                <img src={process.env.PUBLIC_URL + "/logo/LI-In-Bug.png"} class="contactLogo"></img>
               </div>
             </div>
 
-            <div class="row contactContent">
-              <div class="col-12">
+            <div class="row contactContent d-flex justify-content-between">
+              <div class="col-6">
                 <h4>
-                  Location: Québec City, Québec, Canada
+                  Based in: Québec City, Québec, Canada
                 </h4>
+              </div>
+              <div class="col-6 d-flex justify-content-end">
+              <img src={process.env.PUBLIC_URL + "/logo/locationLogo.png"} class="contactLogo"></img>
               </div>
             </div>
 
-            <div class="row contactContent">
-              <div class="col-12">
+            <div class="row contactContent d-flex justify-content-between">
+              <div class="col-6">
                 <h4>
-                  Language: French or english
+                  Language: Very fluent in french and in english
                 </h4>
+              </div>
+              <div class="col-6 d-flex justify-content-end">
+                <img src={process.env.PUBLIC_URL + "/logo/languageLogo.png"} class="contactLogo"></img>
               </div>
             </div>
 
-            <div class="row contactText">
-              <div class="col-12 ">
-                <p> Do not hesitate to contact me, it will be my pleasure to discuss with you about IT opportunities and more!</p>
-              </div>
-            </div>
 
           </div>
 
