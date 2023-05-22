43292b37d3019727fededbab47e39e6bb0629a90
T-Hamel94 <tristan_hamel@hotmail.com>
2023-05-22 17:55:23 -0400

    Made the Post Article work from React and made some ajutement to the article page

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Axios from 'axios';
 
 function Admin() {
-  return (
+    const [state, setState] = useState({ 
+        BlogPostLanguageID: 1,
+        Title: '',
+        Author: '',
+        Content: ''
+      });
+
+    function handleInputChange(event) {
+        setState({...state, [event.target.name]: event.target.value});
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        try {
+            const response = await Axios.post('https://localhost:7057/api/blogposts', {
+                BlogPostLanguageID: state.BlogPostLanguageID,
+                Title: state.Title,
+                Author: state.Author,
+                Content: state.Content
+            });
+        }
+        catch (error) {
+            console.log('error', error);
+        }
+    }
+
+    return (
     <div className='container'>
         <div className='row mt-3'>
             <div className='col-12 text-center'>
@@ -13,30 +40,30 @@ function Admin() {
             <h2>Adding a blog post</h2>
         </div>
         <div className='row'>
-            <form>
-                <div class="mb-3">
-                        <label for="title" class="form-label">Title</label>
-                        <input type="text" class="form-control" id="title" name="title" required></input>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                        <label htmlFor="Title" className="form-label">Title</label>
+                        <input type="text" className="form-control" id="Title" name="Title" onChange={handleInputChange} required />
                 </div>
-                <div class="mb-3">
-                        <label for="author" class="form-label">Author</label>
-                        <input type="text" class="form-control" id="author" name="author" required></input>
+                <div className="mb-3">
+                        <label htmlFor="Author" className="form-label">Author</label>
+                        <input type="text" className="form-control" id="Author" name="Author" onChange={handleInputChange} required />
                 </div>
-                <div class="mb-3">
-                        <label for="content" class="form-label">Content</label>
-                        <textarea class="form-control" id="content" name="content" rows="5" required></textarea>
+                <div className="mb-3">
+                        <label htmlFor="Content" className="form-label">Content</label>
+                        <textarea className="form-control" id="Content" name="Content" rows="5" onChange={handleInputChange} required></textarea>
                 </div>
 
                 <div className='row'>
                     <div className='col-12 text-center'>
-                        <button type="submit" class="btn btn-primary w-25 p-2">Submit</button>
+                        <button type="submit" className="btn btn-primary w-25 p-2">Submit</button>
                     </div>
                 </div>
             </form>
         </div>
 
     </div>
-  )
+    )
 }
 
 export default Admin;
--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,10 +1,13 @@
+import '../styles/pages/Article.css'
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { useTranslation } from 'react-i18next';
 
 function Article() {
   const { id } = useParams();
   const [article, setArticle] = React.useState(null);
+  const { t } = useTranslation('article');
 
   React.useEffect(() => {
     const fetchArticle = async () => {
@@ -24,13 +27,23 @@ function Article() {
   }
 
   return (
-    <div className='row article'>
-      <div className='col-12 text-center'>
-        <h2 className='mt-3'>{article.title}</h2>
-        <h3 className='mt-3'>{article.author}</h3>
-        <h5 className='mt-3'>{new Date(article.createdDate).toDateString()}</h5>
-        <p className='mt-4'>{article.content}</p>
+    <div className='container'>
+
+      <div className='row m-0' id='articleContainer'>
+        <div className='row'>
+          <h1 className>{article.title}</h1>
+          <div className='col-6' id='articleTitle'>
+            <p><b>Written By: </b> {article.author}</p>
+          </div>
+          <div className='col-6' id='articleDate'>
+            <p>{new Date(article.createdDate).toDateString()}</p>
+          </div>
+        </div>
+        <div className='row'>
+          <p className='mt-3 text-center' id='articleContent'>{article.content}</p>
+        </div>
       </div>
+
     </div>
   );
 }
